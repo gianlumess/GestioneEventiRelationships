bcbc29864ac03057221d11f7e093d1ee8f8927f4
Gianluca Messina <gianluca.messina8201@gmail.com>
2024-08-21 15:24:27 +0200

    entity Location creata

---
+++ src/main/java/gianlucamessina/entities/Location.java
@@ -0,0 +1,55 @@
+package gianlucamessina.entities;
+
+import jakarta.persistence.*;
+
+import java.util.UUID;
+
+@Entity
+@Table(name = "location")
+public class Location {
+    @Id
+    @GeneratedValue
+    private UUID id;
+    @Column(name = "nome")
+    private String nome;
+    @Column(name = "citta")
+    private String citta;
+
+    public Location() {
+    }
+
+    public Location(String nome, String citta) {
+        this.nome = nome;
+        this.citta = citta;
+    }
+
+    public UUID getId() {
+        return id;
+    }
+
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getCitta() {
+        return citta;
+    }
+
+    public void setCitta(String citta) {
+        this.citta = citta;
+    }
+
+    @Override
+    public String toString() {
+        return "Location{" +
+                "id=" + id +
+                ", nome='" + nome + '\'' +
+                ", citta='" + citta + '\'' +
+                '}';
+    }
+}
